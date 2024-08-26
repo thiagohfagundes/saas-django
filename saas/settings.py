d993311bcb93afa9983d9e8b752f4c39e34668db
thiagohfagundes <eng.thiagofagundes@gmail.com>
2024-08-26 15:47:51 -0300

    preparação para Raiway

--- saas/settings.py
+++ saas/settings.py
@@ -26,7 +26,14 @@ SECRET_KEY = 'django-insecure-pjp%k$_v6)y@r+-*0mblxn@*-lb+^lgbyr$1p-1goks#ino$ev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
